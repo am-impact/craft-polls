7a40e4c2c5a9dca20f095c52b9e06a95f5c383ec
Frank Zwiers <f.zwiers@am-impact.nl>
2018-06-29 11:36:08 +0200

    Update version

--- PollsPlugin.php
+++ PollsPlugin.php
@@ -13,8 +13,13 @@ class PollsPlugin extends BasePlugin
 
 	public function getVersion()
 	{
-	    return '1.1.0';
+	    return '1.1.1';
 	}
+
+    public function getSchemaVersion()
+    {
+        return '1.1.0';
+    }
 
 	public function getDeveloper()
 	{
